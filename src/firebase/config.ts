bd62da748c902f269cb0ae2d3a7ff5a8e316e2aa
Franco De La Fuente <FranDLFPp@gmail.com>
2026-03-03 06:48:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8755525247-bca0a",
+  "appId": "1:128538439396:web:480e3669235f79699746f1",
+  "apiKey": "AIzaSyCyU35v8oD4LmqoSmJZTTDYIJacv8PIURc",
+  "authDomain": "studio-8755525247-bca0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "128538439396"
+};
