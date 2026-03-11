352ed0de26244369d382f0de3c1c0564b590b14a
Franco De La Fuente <FranDLFPp@gmail.com>
2026-03-11 07:53:08 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,8 @@
-// This file is no longer needed as Firebase has been removed.
-export {};
+export const firebaseConfig = {
+  "projectId": "studio-8755525247-bca0a",
+  "appId": "1:128538439396:web:480e3669235f79699746f1",
+  "apiKey": "AIzaSyCyU35v8oD4LmqoSmJZTTDYIJacv8PIURc",
+  "authDomain": "studio-8755525247-bca0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "128538439396"
+};
